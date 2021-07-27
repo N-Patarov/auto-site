afa6df9b063ba3302a043853e9a6b9b417a8a7d5
N-Patarov <nikolayp1707@gmail.com>
2021-07-27 15:51:33 +0300

    get-screenshto-and-name-of-websites

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import selenium
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-import os
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 
-#content=[description,name]
+
 
 browser = webdriver.Firefox()
 
@@ -12,6 +14,7 @@ browser = webdriver.Firefox()
 def main ():
     setup()
     search()
+    get_info()
 
 
 def setup():
@@ -28,12 +31,33 @@ def setup():
     browser.find_element_by_name("admin[password]").send_keys(admin_password + Keys.ENTER)
     browser.switch_to.window(browser.window_handles[-1])
 
+
 def search():
     search=input("type what websites you need to add: ")
     searchbar=browser.find_element_by_name("q")
     searchbar.send_keys(search,Keys.ENTER)
 
+    link = WebDriverWait(browser, 10).until(
+        EC.presence_of_element_located((By.TAG_NAME, "cite"))
+    )
+    print(link)
+    link.click()
+
+
+def get_info():
+    url=browser.current_url
+    #removes the https:// and the / of the url
+    #to get just the domain of the website
+    link=url.split("https://")
+    link1=link[1].split("/")
+    real_link=link1[0]
+    print(real_link)
+    screenshot=browser.save_screenshot("(" + real_link + ")" + ".png")
+
+    content=[]
+    content.append(real_link)
+    
 
 if __name__ == '__main__':
     main()
-    
+ 
